2f7111663bfc9daa0102b272c50fa7718d6c8e7c
juan <jupegarnica@gmail.com>
2019-11-11 12:41:37 +0100

    password validation on register working

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -6,6 +6,37 @@ const registerController = (req, res) => {
   const user = req.body;
   const db = JSON.parse(fs.readFileSync('./db.json', 'utf-8'));
 
+
+  const longitudPattern = /.{8,}/
+
+  if (!longitudPattern.test(user.password)) {
+     return res
+       .status(400)
+       .json({ message: `password too short` });
+  }
+
+   const digitCharacters = /\d/;
+
+   if (!digitCharacters.test(user.password)) {
+     return res
+       .status(400)
+       .json({ message: `password must have a digit` });
+   }
+    const capitalCharacters = /[A-Z]/;
+
+    if (!capitalCharacters.test(user.password)) {
+      return res
+        .status(400)
+        .json({ message: `password must have a capitalCase` });
+    }
+     const expecialCharacters = /[!|@#$%¬/()\[\]\+\*\{\}]/;
+
+     if (!expecialCharacters.test(user.password)) {
+       return res
+         .status(400)
+         .json({ message: `password must have a special character` });
+     }
+
   const userExists = db.users.some(
     existentUser => existentUser.email === user.email,
   );
