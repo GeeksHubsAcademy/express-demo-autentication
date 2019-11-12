c2555b324860217da12cd156d59ee3936fb07b14
juan <jupegarnica@gmail.com>
2019-11-12 10:15:43 +0100

    log to disk middleware created

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-const express = require('express');
-const registerController = require('./controllers/registerController');
-const loginController = require('./controllers/loginController');
-const logoutController = require('./controllers/logoutController');
-const profileController = require('./controllers/profileController');
+const express = require("express");
+const registerController = require("./controllers/registerController");
+const loginController = require("./controllers/loginController");
+const logoutController = require("./controllers/logoutController");
+const profileController = require("./controllers/profileController");
+
+const loggingMiddleware = require('./middlewares/loggingMiddleware')
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/me', profileController);
-
-app.post('/register', registerController);
-
-app.post('/login',  loginController);
-
-app.get('/logout', logoutController);
 
 
-app.listen(3000, () => console.log('app listening in locahost:3000'));
+app.use(loggingMiddleware);
+
+app.get("/me", profileController);
+
+app.post("/register", registerController);
+
+app.post("/login", loginController);
+
+app.get("/logout", logoutController);
+
+app.listen(3000, () => console.log("app listening in localhost:3000"));
